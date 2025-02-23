c97d746390ccff6a8d4c4b38f0d8f74a2aef8bb6
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-02-22 20:55:09 -0700

    fix: build and gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,8 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,10 +18,10 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist", // Ensures build output goes to "dist/"
+    outDir: "dist",
     rollupOptions: {
-      input: "src/main.tsx", // Ensures Vite knows your entry file
+      input: "index.html",
     },
   },
-  base: "/inventory/", // Replace with your GitHub repo name
+  base: "/inventory/",
 }));
