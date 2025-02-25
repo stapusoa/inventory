1073a20cc434b31d8a26adf510c3fc7bd532f50c
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-02-24 23:03:28 -0700

    change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from "./pages/Index";
 import PartDetails from "./pages/PartDetails";
 import NotFound from "./pages/NotFound";
